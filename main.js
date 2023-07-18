8d95ae330ad651025e4cfa7f8b4f10740e4a5a59
無免ライダー <106133707+mumenrider21@users.noreply.github.com>
2023-07-18 14:02:26 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,9 @@ console.log("main.js!!");
         //2つ目の入力ボックスの値を取得する
         var value2 = document.getElementById("num2").value;
 
-        var value3 = document.getElementById("num3").value;
         //足算する
-        var result = parseInt(value1)  + parseInt(value2) ;
-        var result_syouhi = result + (result*value3*0.01);
+        var result = parseInt(value1) + (value1 * (value2 * 0.01));
+
         //値を設定する
-        document.querySelector('.result').innerHTML = result_syouhi ;
+        document.querySelector('.result').innerHTML = result;
     }
